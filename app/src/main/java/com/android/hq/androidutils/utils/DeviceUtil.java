8ab8b35063d172c7e0fa8c2f3228a7ad31d44146
何强 <heqiang@meizu.com>
2020-05-15 18:35:10 +0800

    add cutout screen method

--- app/src/main/java/com/android/hq/androidutils/utils/DeviceUtil.java
+++ app/src/main/java/com/android/hq/androidutils/utils/DeviceUtil.java
@@ -4,8 +4,17 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Point;
 import android.graphics.Rect;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.util.DisplayMetrics;
+import android.view.DisplayCutout;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowInsets;
 import android.view.WindowManager;
+
+import java.util.List;
 
 /**
  * Created by heqiang on 17-6-20.
@@ -73,5 +82,82 @@ public class DeviceUtil {
         WindowManager windowManager = (WindowManager) context.getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
         windowManager.getDefaultDisplay().getRealMetrics(outMetrics);
         return outMetrics.heightPixels;
+    }
+
+    @RequiresApi(Build.VERSION_CODES.P)
+    public static boolean isCutoutScreen(@NonNull Context context, @NonNull Window window) {
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.P) {
+            // Android P 以下参考各家手机厂商自己实现
+            return false;
+        } else {
+            View decorView = window.getDecorView();
+            if (decorView == null) {
+                return false;
+            }
+
+            WindowInsets rootWindowInsets = decorView.getRootWindowInsets();
+
+            if (rootWindowInsets == null) {
+                return false;
+            }
+
+            DisplayCutout displayCutout = rootWindowInsets.getDisplayCutout();
+
+            if (displayCutout == null) {
+                return false;
+            }
+
+            List<Rect> boundingRects = displayCutout.getBoundingRects();
+            if (boundingRects == null || boundingRects.size() <= 0) {
+                return false;
+            }
+            return true;
+
+            /* 这种方式也ok
+            boolean hascutout = false;
+            try {
+                final Resources res = context.getResources();
+                final int resId = res.getIdentifier("config_mainBuiltInDisplayCutout", "string", "android");
+                final String spec = resId > 0 ? res.getString(resId) : null;
+                hascutout = spec != null && !TextUtils.isEmpty(spec);
+            } catch (Exception e) {
+                Log.w("fish", "Can not update hasDisplayCutout. " +
+                        e.toString());
+            }
+            return hascutout;
+             */
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.P)
+    public static int getCutoutHeight(@NonNull Context context, @NonNull Window window) {
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.P) {
+            // Android P 以下参考各家手机厂商自己实现
+            return 0;
+        } else {
+            View decorView = window.getDecorView();
+            WindowInsets rootWindowInsets = decorView.getRootWindowInsets();
+            if (rootWindowInsets == null) {
+                return 0;
+            }
+            DisplayCutout displayCutout = rootWindowInsets.getDisplayCutout();
+            if (displayCutout == null) {
+                return 0;
+            }
+            return displayCutout.getSafeInsetTop();
+        }
+    }
+
+    // 全屏模式下填充异形屏的区域
+    @RequiresApi(Build.VERSION_CODES.P)
+    public static void adapterCutoutScreen(@NonNull Context context, @NonNull Window window) {
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.P) {
+            // Android P 以下参考各家手机厂商自己实现
+        } else {
+            window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            WindowManager.LayoutParams lp = window.getAttributes();
+            lp.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+            window.setAttributes(lp);
+        }
     }
 }
